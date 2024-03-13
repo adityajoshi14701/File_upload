c8915dee6fbe371e16f62452c6bddd9e2ec5ef8f
Aditya Joshi <adityajoshi14072001@gmail.com>
2024-03-13 11:00:21 +0530

    upadated index.js file , install cloudinary file

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 app.use(express.json())
+const fileupload = require("express-fileupload");
 
+app.use(fileupload());
 
 const dbConnect = require('./config/database');
 dbConnect();
 
 const cloud = require('./config/cloud')
-cloud();
+cloud.cloudinaryConnect();
 
 const fileUploadRoutes = require('./routes/fileUpload')
-app.use('app/v1',fileUploadRoutes);
+app.use('app/v1/upload',fileUploadRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log(`App is running at ${PORT}`)
+})
